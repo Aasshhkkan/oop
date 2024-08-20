aa5f7842c379badb5449154b9309c3b3a1a7586b
ashkan <ashkan.sh5279@gmail.com>
2024-08-20 21:28:09 +0330

    add products

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function render (){
-    console.log("load");
+import products from "./models/products.js";
+import { fechData } from "./utils/httpreq.js"
+const productsNode=document.getElementById("products");
+async function render (){
+   const productData=await fechData();
+   const productsInstance= new products(productsNode,productData);
+   productsInstance.showProducts();
+
 }
 document.addEventListener("DOMContentLoaded", render)
