778a18fefe8623263018bd1a50ec20b9456f538e
ashkan <ashkan.sh5279@gmail.com>
2024-08-21 18:36:10 +0330

    added image

--- models/products.js
+++ models/products.js
@@ -2,18 +2,43 @@ class products {
     constructor(parent,products){
         this.parent=parent;
         this.products=products;
+        this.parent.addEventListener("click",this);
     }
     showProducts(){
         this.products.forEach(product => this.createdcard(product));
     }
     createdcard(data){
         const cardEle = document.createElement("div");
-        const img =document.createElement("img");
-        img.src= data.image;
-        img.alt= data.alt;
-
-        cardEle.appendChild(img);
+        const imgEle = this.productImg(data); 
+        const infoEle = this.productInfo(data);
+        cardEle.innerHTML=imgEle;
+        cardEle.innerHTML+=infoEle; 
         this.parent.appendChild(cardEle);
+    }
+    productImg(data){
+        const { image , alt} = data;
+        const img = `<img alt=${alt} src=${image}>`;
+        return img;
+    }
+    productInfo(data){
+        const{ id, name , price} = data;
+        const intoJSX=`
+        <div id="product-info">
+        <h3>${name}</h3>
+        <div>
+        <span>${price}</span>
+        <button data-id=${id}>+</button></div>
+        </div>`;
+        return intoJSX
+    }
+    handleEvent(event){
+        const element=event.target;
+        if(element.tagName==="BUTTON"){
+            this.addToCart(element.dataset.id)
+        }
+    }
+    addToCart(id){
+        console.log(id);
     }
 }
 
