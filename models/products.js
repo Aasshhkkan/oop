066c9b2e6041940c331d03dc50713bfcd82d95df
ashkan <ashkan.sh5279@gmail.com>
2024-08-26 13:49:50 +0330

    complete project oop

--- models/products.js
+++ models/products.js
@@ -1,45 +1,51 @@
 class products {
-    constructor(parent,products){
-        this.parent=parent;
-        this.products=products;
-        this.parent.addEventListener("click",this);
+    constructor(parent, products, cart) {
+      this.parent = parent;
+      this.products = products;
+      this.cart = cart;
+      this.parent.addEventListener("click", this);
     }
-    showProducts(){
-        this.products.forEach(product => this.createdcard(product));
+    showProducts() {
+      this.products.forEach((product) => this.createdCard(product));
     }
-    createdcard(data){
-        const cardEle = document.createElement("div");
-        const imgEle = this.productImg(data); 
-        const infoEle = this.productInfo(data);
-        cardEle.innerHTML=imgEle;
-        cardEle.innerHTML+=infoEle; 
-        this.parent.appendChild(cardEle);
+    createdCard(data) {
+      const cardEle = document.createElement("div");
+      const imgEle = this.productImg(data);
+      const infoEle = this.productInfo(data);
+      cardEle.innerHTML = imgEle;
+      cardEle.innerHTML += infoEle;
+  
+      this.parent.appendChild(cardEle);
     }
-    productImg(data){
-        const { image , alt} = data;
-        const img = `<img alt=${alt} src=${image}>`;
-        return img;
+    productImg(data) {
+      const { image, alt } = data;
+      const img = `<img alt=${alt} src=${image}>`; //JSX
+      return img;
     }
-    productInfo(data){
-        const{ id, name , price} = data;
-        const intoJSX=`
-        <div id="product-info">
-        <h3>${name}</h3>
-        <div>
-        <span>${price}</span>
-        <button data-id=${id}>+</button></div>
-        </div>`;
-        return intoJSX
+    productInfo(data) {
+      const { id, name, price } = data;
+      const infoJSX = `
+          <div id="product-info">
+              <h3>${name}</h3>
+              <div>
+                  <span>${price}</span>
+                  <button data-id=${id}>+</button>
+              </div>
+          </div>
+      `;
+      return infoJSX;
     }
-    handleEvent(event){
-        const element=event.target;
-        if(element.tagName==="BUTTON"){
-            this.addToCart(element.dataset.id)
-        }
+    handleEvent(event) {
+      const element = event.target;
+      if (element.tagName === "BUTTON") {
+        this.addToCart(element.dataset.id);
+      }
     }
-    addToCart(id){
-        console.log(id);
+    addToCart(id) {
+      const product = this.products.find((i) => i.id === +id);
+      this.cart.products.push(product);
+      this.cart.showProducts();
     }
-}
-
-export default products;
+  }
+  export default products;
+  
--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+import { fechData } from "./utils/httpReq.js";
 import products from "./models/products.js";
-import { fechData } from "./utils/httpreq.js"
-const productsNode=document.getElementById("products");
-async function render (){
-   const productData=await fechData();
-   const productsInstance= new products(productsNode,productData);
-   productsInstance.showProducts();
+import Cart from "./models/cart.js";
 
+const productsNode = document.getElementById("products");
+const cartListNode = document.getElementById("cart-list");
+const totalPriceNode = document
+  .getElementById("total-price")
+  .querySelector("span");
+
+async function render() {
+  const productsData = await fechData();
+  const cartInstance = new Cart(cartListNode, totalPriceNode);
+  const productInstance = new products(productsNode, productsData,cartInstance);
+  console.log(cartInstance);
+  productInstance.showProducts();
 }
-document.addEventListener("DOMContentLoaded", render)
+
+document.addEventListener("DOMContentLoaded", render);
